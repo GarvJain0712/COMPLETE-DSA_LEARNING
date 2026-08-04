5b5328af97eebdd9935bb9c597f985829d3dda24
Garv Jain <garvjain72005@gmail.com>
2026-08-04 11:36:21 +0530

    Time: 3 ms (49.33%), Space: 32.2 MB (75.54%) - LeetHub

---
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        vector<int > ans;
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+
+        for(int i=0;i<n-1;i++)
+        {
+            int j=i+1;
+                for (int x = nums[i] + 1; x < nums[j]; x++)
+                {
+                    ans.push_back(x);
+                }
+        }
+        return ans;
+    }
+};
