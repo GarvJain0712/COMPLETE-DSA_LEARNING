e10e241eeb85919eba3a73f4e66ab71998fc1f5c
Garv Jain <garvjain72005@gmail.com>
2026-08-04 11:36:31 +0530

    Time: 0 ms (100%), Space: 32.2 MB (75.54%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -8,6 +8,9 @@ public:
         for(int i=0;i<n-1;i++)
         {
             int j=i+1;
+            if (nums[i] == nums[j])
+                    continue;
+
                 for (int x = nums[i] + 1; x < nums[j]; x++)
                 {
                     ans.push_back(x);
